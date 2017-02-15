5088647d603170e89e851f0310070639247cbadf
Stéphane Francel <contact@avine.io>
2017-02-15 16:29:11 +0100

    Imrpove globally

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,27 +1,27 @@
-(function() {
+document.addEventListener('DOMContentLoaded', function() {
   // body class list
   var classList = document.body.classList;
 
   // class name toggler
-  function toggle(e, toggleName, removeName){
+  function toggle(e, toggleName, removeName) {
     e.preventDefault();
     classList[classList.contains(toggleName) ? 'remove' : 'add'](toggleName);
     classList.remove(removeName);
   }
 
   // event delegation handler
-  document.body.addEventListener('click', function(e){
-    if (e.target.classList.contains('avn-toggle-nav')) {
-      toggle(e, 'avn-nav-open', 'avn-links-open');
-    } else if(e.target.classList.contains('avn-toggle-links')) {
-      toggle(e, 'avn-links-open', 'avn-nav-open');
+  document.body.addEventListener('click', function(e) {
+    if (e.target.classList.contains('avn-layout-toggle-nav')) {
+      toggle(e, 'avn-layout-nav-open', 'avn-layout-links-open');
+    } else if (e.target.classList.contains('avn-layout-toggle-links')) {
+      toggle(e, 'avn-layout-links-open', 'avn-layout-nav-open');
     }
   });
 
   // close navigation when click on link (usefull for anchor).
-  document.querySelector('#avn-layout > aside').addEventListener('click', function(e){
+  document.querySelector('#avn-layout-wrap > aside').addEventListener('click', function(e){
     if (e.target.nodeName.toLowerCase() === 'a') {
-      classList.remove('avn-nav-open');
+      classList.remove('avn-layout-nav-open');
     }
   });
-})();
+});
